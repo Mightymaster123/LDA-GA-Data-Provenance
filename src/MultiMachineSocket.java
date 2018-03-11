6d22007fd4b0013ce36baf7feae001c80169cf3f
root <root@localhost.localdomain>
2018-03-11 11:57:53 -0700

    check ip address more accurately in local area network

--- src/MultiMachineSocket.java
+++ src/MultiMachineSocket.java
@@ -13,151 +13,147 @@ import java.util.Enumeration;
 public class MultiMachineSocket {
 	private InetAddress masterAddr = null;
 	private int numSlaves = 0;
-	
-	// id = -1 is master, id >= 0 are slaves 
+
+	// id = -1 is master, id >= 0 are slaves
 	private int id;
-	public int getId(){
+
+	public int getId() {
 		return id;
 	}
+
 	private InetAddress slaveAddr[];
-	
+
 	private int port = 0;
-	
+
 	private ServerSocket masterSockets[] = null;
-	
-	private void setMasterAddr(String line) throws UnknownHostException{
+
+	private void setMasterAddr(String line) throws UnknownHostException {
 		masterAddr = InetAddress.getByName(line);
 	}
-	
-	private void setNumSlaves(String line){
+
+	private void setNumSlaves(String line) {
 		numSlaves = Integer.parseInt(line);
 	}
-	public int getNumSlaves(){
+
+	public int getNumSlaves() {
 		return numSlaves;
 	}
-	
-	private void setId(int i){
+
+	private void setId(int i) {
 		id = i;
 	}
-	
-	private void setOneSlave(int i, String line) throws UnknownHostException{
+
+	private void setOneSlave(int i, String line) throws UnknownHostException {
 		slaveAddr[i] = InetAddress.getByName(line);
 	}
-	
-	private void setPort(int i){
+
+	private void setPort(int i) {
 		port = i;
 	}
-	
-	
-	public void config() throws IOException{
-		BufferedReader b=new BufferedReader(new FileReader("config.txt"));
+
+	public void config() throws IOException {
+		BufferedReader b = new BufferedReader(new FileReader("config.txt"));
 		String line = "";
-		
-		//	1)read master address
+
+		// 1)read master address
 		line = b.readLine();
 		setMasterAddr(line);
 		System.out.println("master address: " + line);
-		
-		
+
 		// 2)read number of slaves
 		line = b.readLine();
 		setNumSlaves(line);
 		System.out.println("number of slaves: " + line);
-		
-		
-		
+
 		// 3)read slave addresses
 		slaveAddr = new InetAddress[numSlaves];
-		for(int i = 0; i < numSlaves; i++){
+		for (int i = 0; i < numSlaves; i++) {
 			line = b.readLine();
 			setOneSlave(i, line);
 			System.out.println("slave " + i + " address: " + line);
 		}
-		
+
 		// 4) set port(port for connection with first slave)
 		line = b.readLine();
-		setPort( Integer.parseInt(line));
+		setPort(Integer.parseInt(line));
 		System.out.println(" first port: " + port);
 	}
-	
-	public Socket[] connect() throws IOException{
-		System.out.println("My IP is: " + InetAddress.getLocalHost().toString());
-		Socket sockets[] = null;
-		
-		
-		// if this machine is master
-		boolean is_master = false;
-		try 
-		{
+
+	boolean isMyIP(InetAddress inetAddress) {
+		try {
 			System.out.println("Full list of Network Interfaces:");
-			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) 
-			{
+			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
 				NetworkInterface intf = en.nextElement();
 				System.out.println("    " + intf.getName() + " " + intf.getDisplayName());
-				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) 
-				{
-					InetAddress inetAddress = enumIpAddr.nextElement();
-					System.out.println("        " + inetAddress.toString());
-					if(masterAddr.equals(inetAddress))
-					{
-						is_master = true;
-						break;
+				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+					InetAddress address = enumIpAddr.nextElement();
+					System.out.println("        " + address.toString());
+					if (inetAddress.equals(address)) {
+						return true;
 					}
 				}
 			}
-		} catch (SocketException e) 
-		{
+		} catch (SocketException e) {
 			System.out.println(" (error retrieving network interface list)");
 		}
-	    
-	    
-    	//if(masterAddr.equals(InetAddress.getByName("localhost")) || masterAddr.equals(InetAddress.getLocalHost())){
-    	if(is_master)
-    	{
-    		setId(-1);
-    		System.out.println("I am master");
-    		// create serverSocket for each slave, and then listen to request from each slave
-    		masterSockets = new ServerSocket[numSlaves];
-    		for(int i = 0; i < numSlaves; i++){
-    			masterSockets[i] = new ServerSocket(port + i);
-    			sockets = new Socket[numSlaves];
-    			
-    			while(true){
-    				sockets[i] = masterSockets[i].accept();
-    				//System.out.println("*****tag****");
-    				if(sockets[i] != null)
-    					break;	
-    			}
-    			System.out.println("connection estblished " + i);
-    		}
-    		return sockets;
-    	}
-    	//else(is slave)
-    	else{
-    		sockets = new Socket[1];
-    		// find out which slave it is
-    		for(int i = 0; i < numSlaves; i++){
-    			if(slaveAddr[i].equals(InetAddress.getByName("localhost")) || slaveAddr[i].equals(InetAddress.getLocalHost())){
-    				setId(i);
-    			}
-    		}
-    		System.out.println("I am slave " + id);
-    		System.out.println("slave " + id + "will try to connect with master");
-    		//System.out.println(InetAddress.getByName("localhost").toString() + "    " + (port + id));
-    		Socket socket = new Socket(masterAddr, port + id);
-    		sockets[0] = socket;
-    		return sockets;
-    	}
+		return false;
 	}
-//	
-//	// tell master one slave have finished the job
-//	public void finishSignal(){
-//		
-//	}
-//	
-//	// send result back to master
-//	public void sendResult(){
-//		
-//	}
-//	
+
+	public Socket[] connect() throws IOException {
+		System.out.println("My IP is: " + InetAddress.getLocalHost().toString());
+		Socket sockets[] = null;
+
+		// if this machine is master
+		// if(masterAddr.equals(InetAddress.getByName("localhost")) ||
+		// masterAddr.equals(InetAddress.getLocalHost())){
+		if (isMyIP(masterAddr)) {
+			setId(-1);
+			System.out.println("I am master");
+			// create serverSocket for each slave, and then listen to request from each
+			// slave
+			masterSockets = new ServerSocket[numSlaves];
+			for (int i = 0; i < numSlaves; i++) {
+				masterSockets[i] = new ServerSocket(port + i);
+				sockets = new Socket[numSlaves];
+
+				while (true) {
+					sockets[i] = masterSockets[i].accept();
+					// System.out.println("*****tag****");
+					if (sockets[i] != null)
+						break;
+				}
+				System.out.println("slave connection estblished " + i);
+			}
+			return sockets;
+		}
+		// else(is slave)
+		else {
+			sockets = new Socket[1];
+			// find out which slave it is
+			for (int i = 0; i < numSlaves; i++) {
+				if (isMyIP(slaveAddr[i])) {
+					setId(i);
+					break;
+				}
+			}
+			System.out.println("I am slave " + id);
+			System.out.println("slave " + id + "will try to connect with master");
+			// System.out.println(InetAddress.getByName("localhost").toString() + " " +
+			// (port + id));
+			Socket socket = new Socket(masterAddr, port + id);
+			sockets[0] = socket;
+			return sockets;
+		}
+	}
+	//
+	// // tell master one slave have finished the job
+	// public void finishSignal(){
+	//
+	// }
+	//
+	// // send result back to master
+	// public void sendResult(){
+	//
+	// }
+	//
 }
