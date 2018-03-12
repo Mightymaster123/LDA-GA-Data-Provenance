3c7e5d92fe1fec367c6978e0e0e87ff1822c4eae
root <root@localhost.localdomain>
2018-03-12 04:11:19 -0700

    population configuration

---
+++ src/population_config.java
@@ -0,0 +1,42 @@
+
+public class population_config {
+	public int number_of_topics;
+	public int number_of_iterations;
+	public double fitness_value;
+
+	public void copy(population_config rhs) {
+		number_of_topics = rhs.number_of_topics;
+		number_of_iterations = rhs.number_of_iterations;
+		fitness_value = rhs.fitness_value;
+	}
+	
+	public void random()
+	{
+		random_topic();
+		random_iteration();
+	}
+	
+	public void random_topic()
+	{
+		number_of_topics = (int) Math.floor(Math.random() * 12 + 3);
+		fitness_value = 0;
+	}
+	
+	public void random_iteration()
+	{
+		number_of_iterations = (int) Math.floor(Math.random() * 1000 + 1);
+		fitness_value = 0;
+	}
+	
+	public void print()
+	{
+		System.out.println("number_of_topics: " + number_of_topics + "  number_of_iterations:" + number_of_iterations + "  fitness_value:" + fitness_value);
+		
+	}
+	
+	public void println()
+	{
+		print();
+		System.out.println("");
+	}
+}
