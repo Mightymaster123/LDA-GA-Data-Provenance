addf0c7c14a35e481fba57f3768895c3cb785c20
DongLiu <liudonggalaxy@gmail.com>
2018-03-17 15:57:37 -0700

    add more statistical results

--- src/geneticLogic.java
+++ src/geneticLogic.java
@@ -12,7 +12,7 @@ import com.google.common.collect.Multimap;
 public class geneticLogic {
 
 	public static final int THREADS_PER_MACHINE = 6;
-	public static final double FITNESS_THRESHHOLD = 0.292;
+	public static final double FITNESS_THRESHHOLD = 0.5;
 
 	public boolean isRunning = true;
 	private PopulationConfig[] mInitialPopulation = null;
