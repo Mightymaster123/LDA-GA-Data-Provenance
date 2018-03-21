880ce7b94b9d866e112b2651c74dc15a971dc2ca
DongLiu <liudonggalaxy@gmail.com>
2018-03-21 00:40:10 -0700

    add more logs.  Change max-fitness from 0.35 to 0.5

--- src/Master.java
+++ src/Master.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Master implements NetworkManager.ReceivedProtocolHandler{
 
-	static final int TEST_COUNT = 10;
+	static final int TEST_COUNT = 2;
 	
 	private DataProvenance mDataProvenance = null;
 	
@@ -43,15 +43,20 @@ public class Master implements NetworkManager.ReceivedProtocolHandler{
 		mDataProvenance = null;
 		NetworkManager.getInstance().sendProtocol_ShutdownProcess();
 
+		System.out.println(	"\n\n" + NetworkManager.getInstance().getMachineCount() + " machines"+
+							"    "+geneticLogic.THREADS_PER_MACHINE+" threads-per-machine"+
+							"    topic-range:["+PopulationConfig.MIN_TOPIC_COUNT+","+PopulationConfig.MAX_TOPIC_COUNT +"]"+
+							"    iteration-range:["+PopulationConfig.MIN_ITERATION_COUNT+","+PopulationConfig.MAX_ITERATION_COUNT+ "]");
+		
 		//print out result
-		System.out.println("\n\n\noriginal_version:");
+		System.out.println("\noriginal_version:");
 		for (int i = 0; i < listResultOriginal.size(); ++i) {
 			ResultStatistics result = listResultOriginal.get(i);
 			if (result != null && NetworkManager.getInstance().isMaster()) {
 				System.out.println(result.to_string(String.format("  %3d.", (i + 1))));
 			}
 		}
-		System.out.println("\n\n\nnew_version:");
+		System.out.println("\nnew_version:");
 		for (int i = 0; i < listResultNew.size(); ++i) {
 			ResultStatistics result = listResultNew.get(i);
 			if (result != null && NetworkManager.getInstance().isMaster()) {
